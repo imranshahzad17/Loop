5d36d60052caa810a7c5092cc678b7a5753f5bdd
imranshahzad17 <imranshahzad3454@gmail.com>
2024-03-04 19:03:53 +0500

    Task Done

---
+++ app.js
@@ -0,0 +1,46 @@
+// let num = 4 ;
+// for (let i = 2 ; i < 9 ; i++){
+// num *= i ;
+// } 
+// console.log(num);
+// ANS / 8 / 24 / 96 / 480 / 2,880 / 20,160 / 161,280 
+
+// =====================================================================================
+
+// let score = 8 ;
+//  for (let i = 8 ; i < 14 ; i++){
+// score += i ;
+//  }
+//  console.log(score);
+// ANS / 16 / 25 / 35 / 46 / 58 / 71  
+
+// =====================================================================================
+
+// Print all odd number from 0 to 100
+
+// for( let i = 0; i <= 100 ; i++){
+//     if(i%2  == 1){
+//         console.log(i);
+//     }
+// }
+
+// =====================================================================================
+
+
+// let city = prompt("Enter Your City")
+// let cities = ["karachi","lahore","multan","Rawalpindi","quetta",]
+// let found = false;
+
+// for(let i = 0 ; i < cities.length ; i++){
+//  if(city.toLowerCase() === cities[i].toLowerCase()){
+//     console.log(city,"City Found");
+//     found = true;
+//     break;
+//  }
+// }
+
+// if(!found){
+//     console.log(city , "City Not Found");
+// }
+
+// =====================================================================================
